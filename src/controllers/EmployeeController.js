8fafd93f647a115349a0a533059fb37dfca820b8
devaartana <devaartana11@gmail.com>
2025-06-08 15:08:09 +0700

    fix: little change on controller

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -6,17 +6,21 @@ const employeeModel = require('../models/EmployeeModel')
 const router = express.Router();
 
 const loginEmployee= async (req, res) => {
-    const {id, password} = req.body;
-    if (!id || !password) {
-       res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
-    }
     
     try {
+        const {id, password} = req.body;
+        
+        if (!id || !password) {
+           res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
+           return;
+        }
+
         const employee = await employeeModel.getByID(id)
 
         const isPasswordValid = await bcrypt.compare(password, employee.password);
         if(!isPasswordValid) {
             res.status(401).json(response.buildResponseFailed("invalid password", "invalid password", null));
+            return;
         }
 
         const token = jwt.generateToken(employee.id, "employee")
@@ -27,8 +31,10 @@ const loginEmployee= async (req, res) => {
         }
 
         res.status(200).json(response.buildResponseSuccess("logged in successfully", result))
+        return;
     } catch(error) {
         res.status(500).json(response.buildResponseFailed("failed to login", error.message, null));
+        return;
     }
 }
 
--- src/controllers/PersonalTrainerController.js
+++ src/controllers/PersonalTrainerController.js
@@ -6,18 +6,20 @@ const {authenticate, authorize} = require('../middleware/Authentication')
 const personalTrainerModel = require('../models/PersonalTrainerModel');
 const router = express.Router();
 
-const loginPersonalTrainer = async (req, res) => {
-    const {id, password} = req.body;
-    if (!id || !password) {
-       res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
-    }
-    
+const loginPersonalTrainer = async (req, res) => {    
     try {
+        const {id, password} = req.body;
+        if (!id || !password) {
+        res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
+        return;
+        }
+
         const personalTrainer = await personalTrainerModel.getByID(id)
 
         const isPasswordValid = await bcrypt.compare(password, personalTrainer.password);
         if(!isPasswordValid) {
             res.status(401).json(response.buildResponseFailed("invalid password", "invalid password", null));
+            return;
         }
 
         const token = jwt.generateToken(personalTrainer.id, "trainer")
@@ -27,31 +29,38 @@ const loginPersonalTrainer = async (req, res) => {
             role: "trainer"
         }
 
-        res.status(200).json(response.buildResponseSuccess("logged in successfully", result))
+        res.status(200).json(response.buildResponseSuccess("logged in successfully", result));
+        return;
     } catch(error) {
         res.status(500).json(response.buildResponseFailed("failed to login", error.message, null));
+        return;
     }
 }
 
 const addPersonalTrainer = async (req, res) => {
-    const employeeID = req.id
-    const {name, alamat, password, telephone, gender, price_per_hour} = req.body;
-
-    if (!name || !alamat || !password || !telephone || !gender || !price_per_hour) {
-        res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
-    }
-
-    hashedPassword = await bcrypt.hash(password, 10);
     try {
+        const employeeID = req.id
+        const {name, alamat, password, telephone, gender, price_per_hour} = req.body;
+
+        if (!name || !alamat || !password || !telephone || !gender || !price_per_hour) {
+            res.status(400).json(response.buildResponseFailed("missing required fields", "invalid request body", null));
+            return;
+        }
+
+        hashedPassword = await bcrypt.hash(password, 10);
+        
         const newPersonalTrainer = await personalTrainerModel.create(name, alamat, hashedPassword, telephone, gender, price_per_hour, employeeID);
         
         if(!newPersonalTrainer) {
             res.status(500).json(response.buildResponseFailed("failed create user", "something wrong", null));
+            return;
         }
         
         res.status(201).json(response.buildResponseSuccess("personal trainer created successfully", req.body));
+        return;
     } catch(error) {
         res.status(500).json(response.buildResponseFailed("failed create user", error.message, null));
+        return;
     }
 }
 
