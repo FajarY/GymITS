65252a14ad90b28d65554e4acd2b7aca7a2f479b
Daijin69 <ryanfikri.re@gmail.com>
2025-06-13 02:33:42 +0700

    fix: fix request script

--- public/requestScript.js
+++ public/requestScript.js
@@ -19,36 +19,49 @@ async function userLogin(email, password) {
     const req = {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type" : "application/json"
         },
         body: JSON.stringify({
-            'email': email,
-            'password' : password
+            "email": email,
+            "password" : password
         })
     };
 
-    return await tryFetchJson('/login', req)
+    return await tryFetchJson("/customer/login", req)
 }
 
-export function userRegister(fullname, gender, email, password ) {
+async function userRegister(name, gender, email, password) {
+    const req = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "gender" : gender,
+            "email": email,
+            "password": password
+        })
+    };
+
+    return await tryFetchJson("/customer/register", req);
+}
+
+async function getUserProfile() {
     const req = {
         method: "GET",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type" : "application/json"
+
         },
-        body: JSON.stringify({
-            'fullname': fullname,
-            'gender' : gender,
-            'email': email,
-            'password': password
-        })
     };
 
-    return tryFetchJson('/customer/register', req)
+    return await tryFetchJson("/customer/profile", req);
 }
 
 export{
     tryFetchJson,
     userLogin,
     userRegister,
+    getUserProfile,
 }
