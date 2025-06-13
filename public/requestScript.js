239ad76df5276d9a1b48122a910b9e30090b335c
Daijin69 <ryanfikri.re@gmail.com>
2025-06-13 10:52:05 +0700

    fix: add function in request script

--- public/requestScript.js
+++ public/requestScript.js
@@ -59,9 +59,70 @@ async function getUserProfile() {
     return await tryFetchJson("/customer/profile", req);
 }
 
+async function loginEmployee(employee_id, password) {
+    const req = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            "id": employee_id,
+            "password" : password
+        })
+    };
+
+    return await tryFetchJson("/employee/login", req);
+}
+
+async function getEmployeeProfile() {
+    const req = {
+        method: "GET",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+    };
+
+    return await tryFetchJson("/employee/profile", req);
+}
+
+async function loginTrainer(trainer_id, password) {
+    const req = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            "id": trainer_id,
+            "password" : password
+        })
+    };
+
+    return await tryFetchJson("/personaltrainer/login", req);
+}
+
+async function getTrainerProfile() {
+    const req = {
+        method: "GET",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+    };
+
+    return await tryFetchJson("/personaltrainer/profile", req);
+}
+
+
+
 export{
     tryFetchJson,
+
     userLogin,
     userRegister,
     getUserProfile,
+
+    loginEmployee,
+    getEmployeeProfile,
+
+    loginTrainer,
+    getTrainerProfile,
 }
